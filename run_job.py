b93011bdb955faed138d50df1250a860dfb076a4
arandhaw <arandhawa314@gmail.com>
2024-06-24 16:56:33 +0000

    smol

---
+++ run_job.py
@@ -0,0 +1,35 @@
+from ray.job_submission import JobSubmissionClient, JobStatus
+import time
+
+# 5 states: PENDING, RUNNING, SUCCEEDED, FAILED, STOPPED
+# loops until a state in status_to_wait_for is reached
+def wait_until_status(job_id, status_to_wait_for):
+    status = client.get_job_status(job_id)
+    print(f"status: {status}")
+    while True:
+        previous_status = status
+        status = client.get_job_status(job_id)
+        if previous_status != status:
+            print(f"status: {status}")
+        if status in status_to_wait_for:
+            print(f"Exiting...")
+            break
+        time.sleep(1)
+
+if __name__ == '__main__':
+    # If using a remote cluster, replace 127.0.0.1 with the head node's IP address.
+    client = JobSubmissionClient("http://127.0.0.1:8265")
+    job_id = client.submit_job(
+        # Entrypoint shell command to execute
+        entrypoint="python train.py",
+        # Path to the local directory that contains the script.py file
+        runtime_env={"working_dir": "./"}
+    )
+    print(job_id)
+    try:
+        wait_until_status(job_id, {JobStatus.SUCCEEDED, JobStatus.FAILED, JobStatus.STOPPED})
+    except KeyboardInterrupt:
+        print("Killing job")
+        client.stop_job(job_id) 
+    logs = client.get_job_logs(job_id)
+    print(logs)
---
+++ train-ray.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# coding: utf-8
+#get_ipython().run_line_magic('matplotlib', 'inline')
+#get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
+import ray
+import os
+import subprocess
+import logging
+
+import matplotlib.pyplot as plt
+import time
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
+output_dir = "/home/arand/cat-v-dog-classifier-pytorch/results"
+
+# function to run bash commands
+# print determines whether output is printed
+def bash(command, show_result = True):
+    try:
+        print("Running:", command)
+        result = subprocess.run(command.split(), capture_output=True, text=True, check=True)
+        if show_result:
+            print(result.stdout)
+    except Exception as e:
+        print("Error occured during:", command)
+        print("Error message:\n")
+        print(e)
+
+# loads data on remote machine and returns directory
+def load_data():
+    if not os.path.exists("/cat_dog/training_data"):
+        bash("sudo mkdir /cat_dog")
+        bash("sudo chmod ugo+rwx /cat_dog")
+        bash("sudo gsutil cp -r gs://yakoa-model-data/Cats_and_dogs/* /cat_dog")
+        bash("sudo unzip /cat_dog/training_data.zip -d /cat_dog", show_result = False)
+    if not os.path.exists("/cat_dog/training_data"):
+        print("ERROR!!!!! The data directory doesn't exist")
+    return "/cat_dog/training_data"
+
+@ray.remote(num_gpus = 1)
+def train_model():
+    data_dir = load_data()
+    print(os.listdir(data_dir))
+
+    # Define transforms for the training data and testing data
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                        transforms.RandomResizedCrop(224),
+                                        transforms.RandomHorizontalFlip(),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406],
+                                                                [0.229, 0.224, 0.225])])
+
+    test_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+
+    # Pass transforms in here, then run the next cell to see how the transforms look
+    train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+    test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+    # Needed to load data
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+    testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
+
+    # We can load in a model such as [DenseNet](http://pytorch.org/docs/0.3.0/torchvision/models.html#id5). Let's print out the model architecture so we can see what's going on.
+    model = models.densenet121(pretrained=True)
+    # print(model)
+    # This model is built out of two main parts, the features and the classifier. 
+    # The features part is a stack of convolutional layers and overall works as a feature detector 
+    # that can be fed into a classifier. The classifier part is a single fully-connected layer 
+    # `(classifier): Linear(in_features=1024, out_features=1000)`. 
+    # This layer was trained on the ImageNet dataset, so it won't work for our specific problem. 
+    # That means we need to replace the classifier, but the features will work perfectly on their own. 
+    # In general, I think about pre-trained networks as amazingly good feature detectors that can be 
+    # used as the input for simple feed-forward classifiers.
+
+
+
+    # With our model built, we need to train the classifier. However, now we're using a **really deep** neural network. If you try to train this on a CPU like normal, it will take a long, long time. Instead, we're going to use the GPU to do the calculations. The linear algebra computations are done in parallel on the GPU leading to 100x increased training speeds. It's also possible to train on multiple GPUs, further decreasing training time.
+    # 
+    # PyTorch, along with pretty much every other deep learning framework, uses [CUDA](https://developer.nvidia.com/cuda-zone) to efficiently compute the forward and backwards passes on the GPU. In PyTorch, you move your model parameters and other tensors to the GPU memory using `model.to('cuda')`. You can move them back from the GPU with `model.to('cpu')` which you'll commonly do when you need to operate on the network output outside of PyTorch. As a demonstration of the increased speed, I'll compare how long it takes to perform a forward and backward pass with and without a GPU.
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Training using:", device)
+    model = models.densenet121(pretrained=True)
+
+    # Freeze parameters so we don't backprop through them
+    for param in model.parameters():
+        param.requires_grad = False
+        
+    model.classifier = nn.Sequential(nn.Linear(1024, 512),
+                                    nn.ReLU(),
+                                    nn.Dropout(0.2),
+                                    nn.Linear(512, 256),
+                                    nn.ReLU(),
+                                    nn.Dropout(0.1),
+                                    nn.Linear(256, 2),
+                                    nn.LogSoftmax(dim=1))
+
+    criterion = nn.NLLLoss()
+
+    # Only train the classifier parameters, feature parameters are frozen
+    optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
+
+    model.to(device)
+
+
+    # In[7]:
+
+
+    traininglosses = []
+    testinglosses = []
+    testaccuracy = []
+    totalsteps = []
+    
+    epochs = 1
+    steps = 0
+    running_loss = 0
+    print_every = 5
+    for epoch in range(epochs):
+        for inputs, labels in trainloader:
+            steps += 1
+            if steps > 10: 
+                break
+                
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+            
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+                        
+                        test_loss += batch_loss.item()
+                        
+                        # Calculate accuracy
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+                        equals = top_class == labels.view(*top_class.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                
+                traininglosses.append(running_loss/print_every)
+                testinglosses.append(test_loss/len(testloader))
+                testaccuracy.append(accuracy/len(testloader))
+                totalsteps.append(steps)
+                print(f"Device {device}.."
+                    f"Epoch {epoch+1}/{epochs}.. "
+                    f"Step {steps}.. "
+                    f"Train loss: {running_loss/print_every:.3f}.. "
+                    f"Test loss: {test_loss/len(testloader):.3f}.. "
+                    f"Test accuracy: {accuracy/len(testloader):.3f}")
+                running_loss = 0
+                model.train()
+
+    checkpoint = {
+        'parameters' : model.parameters,
+        'state_dict' : model.state_dict()
+    }
+    # modelref = ray.put(checkpoint)  # doesn't work
+    torch.save(checkpoint, "/mnt/shared/catdogmodel.pth")
+    # return stuff
+    training_record = (traininglosses, testinglosses, testaccuracy, totalsteps)
+    return training_record
+
+# training_record = ray.get(train_model.remote())
+#(traininglosses, testinglosses, testaccuracy, totalsteps) = training_record
+
+#plt.plot(totalsteps, traininglosses, label='Train Loss')
+#plt.plot(totalsteps, testinglosses, label='Test Loss')
+#plt.plot(totalsteps, testaccuracy, label='Test Accuracy')
+#plt.legend()
+#plt.grid()
+
+
+#plt.savefig("/mnt/shared/training_progress.png")
+
+savedir = '/home/arand/cat-v-dog-classifier-pytorch/results/'
+print(os.path.exists("/mnt/shared/training_progress.png"))
+print(os.path.exists(savedir))
+
+bash(f"sudo mv /mnt/shared/training_progress.png {savedir}")
+
+bash(f"sudo mv /mnt/shared/catdogmodel.pth {savedir}")
